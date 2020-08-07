49667a44990b5067d9486998303f2dabf7835415
VenancioFelipe <phelipeatr@gmail.com>
2020-08-07 19:55:39 -0300

    Favorites full component

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import PageHeader from '../../components/PageHeader';
 
 import styles from './styles';
 
 const Favorites = () => {
+
+    const [favorites, setFavorites] = useState([]);
+
+    const loadFavorites = () => {
+        AsyncStorage.getItem('favorites').then(res => {
+            if (res) {
+                const favoriteTeachers = JSON.parse(res);
+                setFavorites(favoriteTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
     return (
         <View style={styles.container}>
             <PageHeader title="Meus proffys favoritos" />
+
+            <ScrollView
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}
+            >
+                {
+                    favorites.map((teacher: Teacher) => (
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorite
+                        />
+                    ))
+                }
+            </ScrollView>
         </View>
     );
 }
