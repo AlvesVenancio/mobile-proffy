84863eb0e80169b316c736d1aaa2d4a2c824f2be
VenancioFelipe <phelipeatr@gmail.com>
2020-08-07 17:36:48 -0300

    connections count

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
+
+import styles from './styles';
 
 import LandingImage from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
-import styles from './styles';
-
 const Landing = () => {
 
     const navigation = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(res => {
+            const { total } = res.data;
+            setTotalConnections(total);
+        });
+    }, []);
+
 
     const handleNavigateToGiveClassesPage = () => {
         navigation.navigate("GiveClasses");
@@ -52,7 +63,7 @@ const Landing = () => {
             </View>
 
             <Text style={styles.totalConnection}>
-                Total de 285 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
