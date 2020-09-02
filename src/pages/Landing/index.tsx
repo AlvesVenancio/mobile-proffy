18913b93f8d663e6bf40be5e32046a5be4fa727a
VenancioFelipe <phelipeatr@gmail.com>
2020-09-01 21:03:24 -0300

    update Landing layout

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
 import api from '../../services/api';
-
-import styles from './styles';
 
 import LandingImage from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import logout from '../../assets/images/icons/logout.png';
+
+import styles from './styles';
 
 const Landing = () => {
 
@@ -24,7 +25,6 @@ const Landing = () => {
         });
     }, []);
 
-
     const handleNavigateToGiveClassesPage = () => {
         navigation.navigate("GiveClasses");
     }
@@ -34,40 +34,58 @@ const Landing = () => {
     }
 
     return (
-        <View style={styles.container}>
-            <Image source={LandingImage} style={styles.banner} />
+        <ScrollView style={styles.container}>
 
-            <Text style={styles.title}>
-                Seja bem-vindo, {'\n'}
-                <Text style={styles.titleBold}>O que deseja fazer?</Text>
-            </Text>
-
-            <View style={styles.buttonsContainer}>
-
-                <RectButton
-                    style={[styles.button, styles.buttonPrimary]}
-                    onPress={handleNavigateToStudyPages}
-                >
-                    <Image source={studyIcon} />
-                    <Text style={styles.buttonText}>Estudar</Text>
-                </RectButton>
-
-                <RectButton
-                    onPress={handleNavigateToGiveClassesPage}
-                    style={[styles.button, styles.buttonSecondary]}
-                >
-                    <Image source={giveClassesIcon} />
-                    <Text style={styles.buttonText}>Dar aulas</Text>
-                </RectButton>
-
+            <View style={styles.header}>
+                <View style={styles.userLogoutContainer}>
+                    <View style={styles.userContainer}>
+                        <Image style={styles.userPhoto} source={{ uri: "https://avatars3.githubusercontent.com/u/49986077?s=460&u=6e3dcd293025aae55ee0c0fc830155e8dc9a7144&v=4" }} resizeMode="cover" />
+                        <Text style={styles.userName}>Felipe Venancio</Text>
+                    </View>
+                    <RectButton
+                        style={styles.logoutButton}
+                        onPress={() => { }}
+                    >
+                        <Image source={logout} />
+                    </RectButton>
+                </View>
+                <Image source={LandingImage} style={styles.banner} />
             </View>
 
-            <Text style={styles.totalConnection}>
-                Total de {totalConnections} conexões já realizadas {' '}
-                <Image source={heartIcon} />
-            </Text>
-        </View>
+            <View style={styles.body}>
+                <Text style={styles.title}>
+                    Seja bem-vindo, {'\n'}
+                    <Text style={styles.titleBold}>O que deseja fazer?</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+
+                    <RectButton
+                        style={[styles.button, styles.buttonPrimary]}
+                        onPress={handleNavigateToStudyPages}
+                    >
+                        <Image style={styles.buttonImg} source={studyIcon} />
+                        <Text style={styles.buttonText}>Estudar</Text>
+                    </RectButton>
+
+                    <RectButton
+                        onPress={handleNavigateToGiveClassesPage}
+                        style={[styles.button, styles.buttonSecondary]}
+                    >
+                        <Image style={styles.buttonImg} source={giveClassesIcon} />
+                        <Text style={styles.buttonText}>Dar aulas</Text>
+                    </RectButton>
+
+                </View>
+
+                <Text style={styles.totalConnection}>
+                    Total de {totalConnections} conexões já realizadas {' '}
+                    <Image source={heartIcon} />
+                </Text>
+            </View>
+        </ScrollView>
     );
 }
+
 
 export default Landing;
